a4d06f1b4c35979b001aa1ef54f85d54df76da58
Yeldos <eldosanarbaev7@gmail.com>
2024-05-16 11:19:06 +0500

    Call function with custom context

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-Array.prototype.snail = function(rowsCount, colsCount) {
-    if(rowsCount * colsCount !== this.length) return [];
-    let res = new Array(rowsCount).fill(null).map(() => []);
-    for(let row = 0; row < colsCount; row++){
-        for(let col = 0; col < rowsCount; col++){
-            res[(row & 1) ? rowsCount - 1 - col : col].push(this[row * rowsCount + col]);
-        }
-    }
-    return res;
-};
+Function.prototype.callPolyfill = function(context, ...args) {
+    const symbolId = Symbol();
+    context[symbolId] = this;
+    return context[symbolId](...args);
+}
 
-const arr = [1,2,3,4];
-const newArr = arr.snail(1,4); // [[1,2,3,4]]
-console.log(newArr);
+function increment() { 
+    this.count++; 
+    return this.count; 
+}
+
+console.log(increment.callPolyfill(
+    {count: 1}
+)); 
