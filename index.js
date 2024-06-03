6531801efad5ec3f90629120842761dcb63457aa
Yeldos <eldosanarbaev7@gmail.com>
2024-06-03 20:41:21 +0500

    TypeScript: 2-7, basic

---
+++ index.js
@@ -0,0 +1,6 @@
+var isBirthday = 1;
+var age = 40;
+var userName = 'John';
+if (isBirthday) {
+    console.log("Congratulations ".concat(userName, ", age: ").concat(age + 1));
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+const isBirthday: boolean = true;
+let age: number = 40;
+const userName: string = 'John';
+
+age = 40;
+
+if(isBirthday) {
+    console.log(`Congratulations ${userName.toUpperCase()}, age: ${age + 1}`);
+}
