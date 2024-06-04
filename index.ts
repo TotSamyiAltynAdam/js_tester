5e41fcb7214fee8f1cbf5080d70ffa4dddb2d21a
Yeldos <eldosanarbaev7@gmail.com>
2024-06-04 17:09:10 +0500

    2-18 Union

--- index.ts
+++ index.ts
@@ -1,34 +1,9 @@
-const userData = {
-    isBirthdayData: true,
-    ageData: 40,
-    userNameData: 'John',
-    messages: {
-        error: 'Error'
-    }
+const message: string | number = 5;
+const messages: string[] | number[] = ['a', 'b'];
+
+function printMsg(msg: string | number): void {
+    console.log(msg);
 }
 
-const userDataTuple: [...boolean[], number, string] = [true, false, 40, 'John'];
-
-const createError = (msg: string): never => {
-    throw new Error(msg);
-}
-
-function logBrthMsg({ //destructurization
-    isBirthdayData, 
-    userNameData, 
-    ageData,
-    messages: {error}
-}: { //anotation
-    isBirthdayData: boolean, 
-    userNameData: string, 
-    ageData: number,
-    messages: {error: string}
-}): string {
-    if(isBirthdayData) {
-        return `Congratulations ${userNameData.toUpperCase()}, age: ${ageData + 1}`;
-    } else {
-        return createError(error);
-    }
-}
-
-console.log(logBrthMsg(userData));
+printMsg(4);
+printMsg('Hello World!');
