1a2aeb5ba64016c514ff1201cf780f3ec23757d6
Yeldos <eldosanarbaev7@gmail.com>
2024-06-04 23:46:49 +0500

    Literals of Objects and Annotation of Functions

--- index.ts
+++ index.ts
@@ -2,22 +2,27 @@ let msg: 'Hello' = 'Hello';
 
 msg = 'Hello';
 
-const port3000: number = 3000;
-const port3001: number = 3001;
+const serverConfig: { 
+    protocol: 'http' | 'https'; 
+    port: 3000 | 3001 
+} = {
+    protocol: 'https',
+    port: 3001
+};
 
-function startServer(
+const startServer: (
     protocol: 'http' | 'https', 
     port: 3000 | 3001
-): 'Server started' {
-    if(port == port3000 || port == port3001){
-        console.log(`Server started on ${protocol}://server:${port}`);
-    } else {
-        console.log(`Invalid port: ${port}`);
-    }
+)=> string = (
+    protocol: 'http' | 'https', 
+    port: 3000 | 3001
+): 'Server started' => {
+    console.log(`Server started on ${protocol}://server:${port}`);
+
     return 'Server started';
 }
 
-startServer('https', 3001);
+startServer(serverConfig.protocol, serverConfig.port);
 
 type AnimationID = string | number;
 type AnimationTimingFunc = 'ease' | 'ease-in' | 'ease-out';
