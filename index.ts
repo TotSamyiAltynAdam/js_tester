12afa05a8594afd7394880d014f455fc11995a09
Yeldos <eldosanarbaev7@gmail.com>
2024-06-04 23:28:17 +0500

    2-20 Literal Types

--- index.ts
+++ index.ts
@@ -1,36 +1,37 @@
-const message: string | number = 5;
-const messages: string[] | number[] = ['a', 'b'];
+let msg: 'Hello' = 'Hello';
 
-function printMsg(msg: string[] | number | boolean): void {
-    if(Array.isArray(msg)) {
-        msg.forEach(msg => console.log(msg));
-    } else if(typeof msg === 'number') {
-        console.log(msg.toFixed());
+msg = 'Hello';
+
+const port3000: number = 3000;
+const port3001: number = 3001;
+
+function startServer(
+    protocol: 'http' | 'https', 
+    port: 3000 | 3001
+): 'Server started' {
+    if(port == port3000 || port == port3001){
+        console.log(`Server started on ${protocol}://server:${port}`);
     } else {
-        console.log(msg);
+        console.log(`Invalid port: ${port}`);
     }
+    return 'Server started';
 }
 
-const printReadings = (a: number | string, b: number | boolean) => {
-    if(a === b) console.log(a + " " + b);
+startServer('https', 3001);
+
+function createAnimation(
+    id: string | number, 
+    animName: string, 
+    timingFunc: 'ease' | 'ease-in' | 'ease-out' = 'ease',
+    duration: number,
+    iterCount: 'infinite' | number
+): void {
+    //const elem = document.querySelector(`#${id}`) as HTMLElement;
+
+    //if(elem) {
+        console.log(`${animName} ${timingFunc} ${duration} ${iterCount}`);
+       // elem.style.animation = `${animName} ${timingFunc} ${duration} ${iterCount}`;
+    //}
 }
 
-const printReadings2 = (a: number[] | string) => {
-    console.log(a.slice(0, 3));
-}
-
-function checkReadings (readings: {system: number} | {user: number}): void {
-    if('system' in readings){
-        console.log(readings.system);
-    } else {
-        console.log(readings.user);
-    }
-}
-
-function logValue(x: string | Date){
-    if(x instanceof Date){
-        console.log(x.getDate());
-    } else {
-        console.log(x.toLowerCase());
-    }
-}
+createAnimation('id', 'animName', 'ease', 5, 'infinite')
