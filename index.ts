e6268b422cf7741ad7511502fe0351cb8b0a2d80
Yeldos <eldosanarbaev7@gmail.com>
2024-06-05 16:24:07 +0500

    3-30 Readonly

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 interface User {
-    login: string;
+    readonly login: string;
     password: string;
     age: number;
     // address?: string;
@@ -17,6 +17,13 @@ const user: User = {
     address: 'Almaty: Abay street 26a'
 };
 
+const userFreeze: Readonly<User> = {
+    login: 'aidos',
+    password: 'qwerty',
+    age: 17,
+    address: 'Almaty: Abay street 26a'
+};
+
 let dbName: string;
 sendUserData(user, 'efvenienf');
 
@@ -26,3 +33,7 @@ function sendUserData(obj: User, db?: string): void{
     dbName = '12344';
     console.log(obj.parents?.father?.toLowerCase(), db?.toLowerCase());
 }
+
+const basicPortsArray: ReadonlyArray<number> = [3000,3001, 5000];
+// const basicPortsArray: readonly number[] = [3000,3001, 5000];
+// const basicPortsTuple: readonly [number, ...string[]] = [3000,'3001', '5000'];
