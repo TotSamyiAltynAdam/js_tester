66d772eb0852f034971227760809263f57834e10
Yeldos <eldosanarbaev7@gmail.com>
2024-06-05 16:07:39 +0500

    3-28 Property Modifiers

--- index.ts
+++ index.ts
@@ -1,73 +1,24 @@
-type validAmount = "empty" | number;
-// структура данных склада с одеждой
-
-interface ClothesWarehouse {
-	jackets: validAmount;
-	hats: validAmount;
-	socks: validAmount;
-	pants: validAmount;
-}
-
-// структура данных склада с канцтоварами
-
-interface StationeryWarehouse {
-	scissors: validAmount;
-	paper: "empty" | boolean;
-}
-
-// структура данных склада с бытовой техникой
-
-interface AppliancesWarehouse {
-	dishwashers: validAmount;
-	cookers: validAmount;
-	mixers: validAmount;
-}
-
-// общая структура данных, наследует все данные из трех выше
-// + добавляет свои
-
-interface TotalWarehouse 
-            extends ClothesWarehouse, 
-                    StationeryWarehouse, 
-                    AppliancesWarehouse {
-	deficit: boolean;
-	date: Date;
-}
-
-// главный объект со всеми данными, должен подходить под формат TotalWarehouse
-
-const totalData: TotalWarehouse = {
-	jackets: 5,
-	hats: "empty",
-	socks: "empty",
-	pants: 15,
-	scissors: 15,
-	paper: true,
-	dishwashers: 3,
-	cookers: "empty",
-	mixers: 14,
-    deficit: true,
-    date: new Date()
+interface User {
+    login: string;
+    password: string;
+    age: number;
+    // address?: string;
+    address: string | undefined;
+    parents?: {
+        mother?: string;
+        father?: string;
+    }
 };
 
-// Реализуйте функцию, которая принимает в себя главный объект totalData нужного формата
-// и возвращает всегда строку
-// Функция должна отфильтровать данные из объекта и оставить только те названия товаров, у которых значение "empty"
-// и поместить их в эту строку. Если таких товаров нет - возвращается другая строка (см ниже)
+const user: User = {
+    login: 'yeldos',
+    password: 'qwerty',
+    age: 21,
+    address: 'Almaty: Abay street 26a'
+};
 
-// С данным объектом totalData строка будет выглядеть:
-// "We need this items: hats, socks, cookers"
-// Товары через запятую, в конце её не должно быть. Пробел после двоеточия, в конце строки его нет.
+const dbName = '12345';
 
-function printReport(data: TotalWarehouse): string {
-    const result = Object.entries(data)
-                                .filter((item) => item[1] === 'empty')
-                                .reduce((res, item) => `${res} ${item[0]},`, "");
-    if(result.trim().length){
-	    return `We need this items:${result.slice(0, -1)}`;
-    } else {
-        return "Everything fine";
-    }
+function sendUserData(obj: User, db?: string): void{
+    console.log(obj.parents?.father?.toLowerCase(), db?.toLowerCase());
 }
-
-console.log(printReport(totalData));
