8f787ddce0db87a5ca7b1237765a30a8cf161b8d
Yeldos <eldosanarbaev7@gmail.com>
2024-06-05 14:19:03 +0500

    3-24 Interfaces

--- index.ts
+++ index.ts
@@ -2,53 +2,56 @@ let msg: 'Hello' = 'Hello';
 
 msg = 'Hello';
 
-type Config = { protocol: 'http' | 'https'; port: 3000 | 3001 };
-type Role = { role: string; };
+//type Config = { protocol: 'http' | 'https'; port: 3000 | 3001 };
+interface IConfig {
+    protocol: 'http' | 'https'; 
+    port: 3000 | 3001;
+    log: (msg: string) => void;
+}
+// type Role = { role: string; };
+// type ConfigWithRole = Config & Role;
+interface IRole {
+    role: string;
+}
 
-type ConfigWithRole = Config & Role;
+interface IConfigWithRole extends IConfig, IRole {}
 
-
-const serverConfig: ConfigWithRole = {
+const serverConfig: IConfigWithRole = {
     protocol: 'https',
     port: 3001,
-    role: 'admin'
+    role: 'admin',
+    log: (msg: string): void => console.log(msg)
 };
 
-const backupConfig: ConfigWithRole = {
-    protocol: 'http',
-    port: 3000,
-    role: 'sysadmin'
-};
+// const backupConfig: ConfigWithRole = {
+//     protocol: 'http',
+//     port: 3000,
+//     role: 'sysadmin'
+// };
 
-type StartFunction = (protocol: 'http' | 'https', port: 3000 | 3001) => string;
+type StartFunction = (
+    protocol: 'http' | 'https', 
+    port: 3000 | 3001, 
+    log: (msg: string) => void
+) => string;
 
 const startServer: StartFunction = (
     protocol: 'http' | 'https', 
-    port: 3000 | 3001
+    port: 3000 | 3001,
+    log: (msg: string) => void
 ): 'Server started' => {
-    console.log(`Server started on ${protocol}://server:${port}`);
+    log(`Server started on ${protocol}://server:${port}`);
 
     return 'Server started';
 }
 
-startServer(serverConfig.protocol, serverConfig.port);
+startServer(serverConfig.protocol, serverConfig.port, serverConfig.log);
 
-type AnimationID = string | number;
-type AnimationTimingFunc = 'ease' | 'ease-in' | 'ease-out';
-
-function createAnimation(
-    id: AnimationID,
-    animName: string,
-    timingFunc: AnimationTimingFunc = 'ease',
-    duration: number,
-    iterCount: 'infinite' | number
-): void {
-    //const elem = document.querySelector(`#${id}`) as HTMLElement;
-
-    //if(elem) {
-        console.log(`${animName} ${timingFunc} ${duration} ${iterCount}`);
-       // elem.style.animation = `${animName} ${timingFunc} ${duration} ${iterCount}`;
-    //}
+interface Styles {
+    [key: string]: string; 
 }
-
-createAnimation('id', 'animName', 'ease', 5, 'infinite');
+const styles: Styles = {
+    position: 'absolute',
+    top: '20px',
+    left: '50px'
+}
